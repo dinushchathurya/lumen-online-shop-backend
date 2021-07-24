9b86b3a522461c3f1f3748a7cfaf9319872c1b17
dinushchathurya <dinushchathurya21@gmail.com>
2021-07-24 18:53:40 +0530

    :hammer: refactor: peering getJWTIdentifier & getJWTCustomClaims methods in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
@@ -49,5 +50,14 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     public function shoppingCart()
     {
         return $this->hasMany(ShoppingCart::class, 'user_id');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
