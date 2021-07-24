c548a080b90763fd2176dcfdbacef8c2bc3d9685
dinushchathurya <dinushchathurya21@gmail.com>
2021-07-24 20:46:13 +0530

    :sparkles:feat: create function to create new category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -25,7 +25,30 @@ class CategoriesController extends Controller
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->only('title'), [
+            'title' => 'required'
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false, 
+                'message' => 'Please fix these errors', 
+                'errors' => $validator->errors()
+            ], 500);
+        }
+
+        $category = new Category();
+        $category->title = $request->input('title');
+        $category->description = $request->input('description');
+        $category->parent_id = $request->input('parent_id') != '' ? $request->input('parent_id') : null;
+        $category->featured = $request->input('featured');
+        $category->save();
+        $this->insertFeatures($request, $category);
+        return response()->json([
+            'success' => true, 
+            'message' => 'Created successfully', 
+            'category' => $category
+        ], 201);
     }
 
     public function show($id)
@@ -52,7 +75,7 @@ class CategoriesController extends Controller
         }if ($request->filter_by_parent_id) {
             $query->where('parent_id', $request->filter_by_parent_id);
         }
-        
+
         $categories = $query->paginate(10);
         return $categories;
     }
