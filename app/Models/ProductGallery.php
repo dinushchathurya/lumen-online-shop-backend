f5ad865fbfb0aead3ff55cd3bd86005827789432
dinushchathurya <dinushchathurya21@gmail.com>
2021-07-24 21:22:24 +0530

    :hammer: refactor: create function to getImageAttribute

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -5,5 +5,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductGallery extends Model
 {
+    use Helpers;
+
     protected $table = "product_gallery";
+    protected $appends = ["image_url"];
+    
+    public function getImageUrlAttribute()
+    {
+        $urls = [];
+        foreach ($this->imagesSizes as $dirName => $imagesSize) {
+            $urls[$dirName] = url('/') . '/uploads/' . $this->product_id . '/' . $dirName . '/' . $this->image;
+        }
+        return $urls;
+    }
 }
